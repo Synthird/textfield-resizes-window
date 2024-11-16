21878287e8b62873b89de2c90973ebe6d0410859
Synthird <105394020+Synthird@users.noreply.github.com>
2024-11-15 22:25:45 -0800

    Resized the window to fit the checkbox text

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -55,12 +55,12 @@ public class MainFrame extends JFrame implements ActionListener {
 
         // Checkbox panel
         JPanel checkPanel = new JPanel();
-        checkPanel.setBounds(0, 35 * 2, 250, 35);
+        checkPanel.setBounds(0, 35 * 2, 310, 35);
         checkPanel.setOpaque(false);
         checkPanel.setLayout(flowLayout);
         this.add(checkPanel);
 
-        resizable = new JCheckBox("Window is resizable via mouse and maximize button");
+        resizable = new JCheckBox("Resizable via mouse and maximize/restore button");
         resizable.setFocusable(false);
         resizable.setOpaque(false);
         resizable.addActionListener(this);
@@ -79,7 +79,7 @@ public class MainFrame extends JFrame implements ActionListener {
         buttonPanel.add(resizeButton);
 
         this.setTitle("textfield resizes window");
-        this.setSize(250, 600);
+        this.setSize(330, 600);
         this.setResizable(false);
         this.setLayout(null);
         this.setLocationRelativeTo(null);
@@ -95,7 +95,7 @@ public class MainFrame extends JFrame implements ActionListener {
                 ySize = Integer.parseInt(yTextField.getText().replaceAll("\\s", ""));
                 if (ySize < 135 && !this.isResizable()) {
                     int chooseToResize = JOptionPane.showConfirmDialog(null,
-                            "Your checkbox is going to be covered since you did not allow this window to be resizable via mouse and maximize button!\nDo you want to resize the window anyway?",
+                            "Resizable via mouse and maximize/restore button is not ticked!\nDo you want to resize the window anyway?",
                             "Cannot resize window when choosing to confirm", JOptionPane.YES_NO_OPTION);
 
                     if (chooseToResize == 0) {
